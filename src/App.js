119ce94acb643a354e0e9449e1722af68ff1ce15
9GAG <sonalanand102@gmail.com>
2022-09-16 13:55:20 +0530

    Stopwatch created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [time, setTime] = useState(0)
+  const [timerOn, setTimerOn] = useState(false)
+
+  useEffect(() => {
+    let interval = null;
+
+    if(timerOn){
+      interval = setInterval(()=>{
+        setTime(prevTime => prevTime + 10)
+      },10)
+    }else{
+      clearInterval(interval)
+    }
+
+    return () => clearInterval(interval)
+  }, [timerOn])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className="stopWatch">
+        <div className='time'>
+          <span className='minutes'>{("0" + Math.floor((time/60000)%60 )).slice(-2)} : </span>
+          <span className='seconds'>{("0" + Math.floor((time/1000)%60)).slice(-2)} : </span>
+          <span className='milliSeconds'>{("0" + ((time/10)%100)).slice(-2)} </span>
+        </div>
+        
+        <div className='buttons'>
+          {!timerOn && time == 0 && (
+            <button className = 'startButton' onClick={() => setTimerOn(true)}>Start</button>
+          )}
+
+          {timerOn && (
+            <button className = 'stopButton' onClick={() => setTimerOn(false)}>Stop</button>
+          )}
+
+          {!timerOn && time !== 0 &&(
+            <button className = 'resumeButton' onClick={() => setTimerOn(true)}>Resume</button>
+          )}
+
+          {!timerOn && time > 0 && (
+            <button className = 'resetButton' onClick={() => setTime(0)}>Reset</button>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
